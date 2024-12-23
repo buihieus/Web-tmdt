f3c67b3e752c75c7b3a3e96c8e034bd9bea4f520
Bui Cong Hieu <buihieu9259@gmail.com>
2024-12-23 16:41:38 +0700

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+const flash = require('express-flash')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const routes = require("./routes/client/index.route")
+const routesAdmin = require("./routes/admin/index.route")
+
+const app = express()
+const port = process.env.PORT; 
+
+app.use(methodOverride('_method'))
+
+const database = require("./config/database");
+database.connect();
+
+const configSystem = require("./config/system");
+
+// parse application/x-www-form-urlencoded (Lấy dữ liệu từ req.body)
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+
+//Flash (Hiện thông báo)
+app.use(cookieParser('VANCHIKHANH'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+
+app.use(express.static(`${__dirname}/public`))
+
+//TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+//App Locals variables (chỉ sử dụng biến toàn cục này trong file pug)
+app.locals.preFixAdmin = configSystem.prefixAdmin;
+
+//Routes
+routes(app);
+routesAdmin(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
