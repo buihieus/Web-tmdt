a5e53e0bc358f4de99c29dce57ce9a25fe56faf9
Bui Cong Hieu <buihieu9259@gmail.com>
2024-12-23 16:31:38 +0700

    homepage & fixadmin

---
+++ routes/client/index.route.js
@@ -0,0 +1,9 @@
+const productRoutes = require("./product.route");
+const homeRoutes = require("./home.route");
+
+//sử dụng trong file index.js chính
+module.exports = (app) => {
+  app.use('/', homeRoutes);
+
+  app.use('/products', productRoutes);
+}
