9f90f14408962f14222e6062a987325912d4181b
GingBaoNo <gingbao26062003@gmail.com>
2025-01-02 00:27:46 +0700

    phan quyen

--- middleware/admin/auth.middleware.js
+++ middleware/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 module.exports.requireAuth = async (req, res, next) => {
     if (!req.cookies.token) {
@@ -9,7 +10,7 @@ module.exports.requireAuth = async (req, res, next) => {
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
         return;
     } else {
-        const user = await Account.findOne({ token: req.cookies.token });
+        const user = await Account.findOne({ token: req.cookies.token }).select("-password");
         if (!user) {
             if (req.path === `${systemConfig.prefixAdmin}/auth/login`) {
                 return next();
@@ -17,6 +18,9 @@ module.exports.requireAuth = async (req, res, next) => {
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
             return;
         } else {
+            const role = await Role.findOne({ _id: user.role_id }).select("title permissions");
+            res.locals.user = user;
+            res.locals.role = role;
             next();
         }
     }
