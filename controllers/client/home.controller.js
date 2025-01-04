d0736bf5bf3c6dfb7b4b4186d4fb73dc720ad464
Bui Cong Hieu <buihieu9259@gmail.com>
2025-01-04 16:29:13 +0700

    fix homepage

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,12 +1,30 @@
 const User = require("../../models/user.model");
+const Products = require("../../models/product.model");
 
 // [GET] /home
 module.exports.index = async (req, res) => {
     // Kiểm tra xem người dùng có đăng nhập không
     const user = req.session.userId ? await User.findById(req.session.userId) : null;
+    // Get list products featured
+    const productsFeatured = await Products.find({
+        deleted: false,
+        featured: 1,
+        status: "active",
+    });
+
+    // Giới hạn số lượng sản phẩm hiển thị (chỉ 8 sản phẩm)
+    const limitedProducts = productsFeatured.slice(0,4);
+    // Get the latest products list
+    const productsNew = await Products.find({
+        deleted: false,
+        status: "active",
+    }).sort({ position: "desc"}).limit(6);
 
     res.render('client/pages/home/index', {
+        categoryName: "SẢN PHẨM NÔI BẬT",
         pageTitle: "Trang chủ",
-        user: user // Truyền biến user vào view
+        user: user, // Truyền biến user vào view
+        productsFeatured: limitedProducts, // Chỉ truyền 8 sản phẩm
+        productsNew: productsNew // Truyền 6 sản phẩm mới nhất
     });
 };
