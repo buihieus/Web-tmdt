924e7c54fa57ed064ab2901056445c55dfbab716
Bui Cong Hieu <buihieu9259@gmail.com>
2025-01-10 15:37:30 +0700

    trang chi tiết bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -50,28 +50,51 @@ module.exports.detail = async (req, res) => {
 };
 
 // [GET] /products/:slugCategory
-// module.exports.category = async (req, res) => {
-//   const category = await ProductCategory.findOne({
-//     slug: req.params.slugCategory,
-//     status: "active",
-//     deleted: false,
-//   });
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false,
+  });
 
-//   const listSubCategory = await productsCategoryHelper.getSubCategory(
-//     category.id
-//   );
+  const listSubCategory = await productsCategoryHelper.getSubCategory(
+    category.id
+  );
 
-//   const listSubCategoryId = listSubCategory.map((item) => item.id);
-//   const products = await Product.find({
-//     product_category_id: { $in: [category.id, ...listSubCategoryId] },
-//     deleted: false,
-//     status: "active",
-//   }).sort({ position: "desc" });
+  const listSubCategoryId = listSubCategory.map((item) => item.id);
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc" });
 
-//   productsHelper.priceNewProducts(products);
+  productsHelper.priceNewProducts(products);
 
-//   res.render("client/pages/products/index.pug", {
-//     pageTitle: category.title,
-//     products: products,
-//   });
-// };
+  res.render("client/pages/products/index.pug", {
+    pageTitle: category.title,
+    products: products,
+  });
+};
+
+// Lấy thông tin chi tiết sản phẩm
+module.exports.getProductDetail = async (req, res) => {
+  try {
+    const productId = req.params.id; // Lấy ID sản phẩm từ URL
+    const product = await Product.findById(productId); // Lấy sản phẩm từ DB
+
+
+    if (!product) {
+      req.flash("error", "Không tìm thấy sản phẩm!");
+      return res.redirect("/"); // Quay về trang chủ nếu không tìm thấy
+    }
+
+
+    res.render("client/pages/product/detail", {
+      product, // Truyền dữ liệu sản phẩm vào template
+    });
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    res.status(500).send("Internal server error");
+  }
+};
+
